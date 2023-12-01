f67ff5bb4b803f850663451acbcf734116bcf8cc
Jay8012 <sanjay.hartmann.12@gmail.com>
2023-12-01 21:40:03 +0100

    Initial Commit

---
+++ demoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demoApp
+//
+//  Created by Sanjay Hartmann on 01.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoApp/demoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoAppApp.swift
+//  demoApp
+//
+//  Created by Sanjay Hartmann on 01.12.23.
+//
+
+import SwiftUI
+
+@main
+struct demoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
